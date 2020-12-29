cd3bedf88323d2f1691e73abb4bdc93db60939fc
itsnotreal675 <76721666+itsnotreal675@users.noreply.github.com>
2020-12-29 15:57:46 -0300

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,27 @@
+const help = () => {
+	return '>```Comandos para fazer figurinhas```<\n' +
+	'comando : *!sticker* ou *!stiker*\n' +
+	'descrição : converta imagens/gifs da ana/video para figurinhas\n' +
+	'como usar : marque a imagem/gif/video, ou envie imagem/gif/video com a legenda descrita acima\n\n' + 
+	'comando : *!toimg*\n' +
+	'descrição : converta figurinhas para imagem\n' +
+	'como usar : marque a figurinha com a legenda descrita acima\n\n' +
+	'comando : *!tsticker* ou *!tstiker*\n' +
+	'descrição : converta texto para figurinha\n' +
+	'como usar : *!tsticker (coloque o texto aqui)\n\n' +
+	
+'> ```Comandos de meme``` <\n' +
+	'comando : *!meme*\n' +
+	'descrição : meme aleatório da gringa [english]\n' +
+	'como usar : basta enviar o comando\n\n' +
+	'comando : *!memebr*\n' +
+	'descrição : memes aleatórios br [pt-br]\n' +
+	'como usar : basta enviar o comando\n\n' +
+	
+'> ```Comando de texto para fala``` <\n' +
+	'comando : *!gtts*\n' +
+	'descrição : conversor de texto para /audio\n' +
+	'uso : *!gtts [cc] [texto]*\nexemplo : *!gtts pt-br Admira brow?*'
+}
+
+exports.help = help
